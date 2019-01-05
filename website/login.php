611c7a01ecccec4734a6de594017bbeb3668f33c
unknown <neel19@somaiya.edu>
2019-01-05 19:44:12 +0530

    made cookies

--- website/login.php
+++ website/login.php
@@ -11,6 +11,7 @@ if(!$conn)
 die("Connection Failed".mysqli_connect_error());
 }
 
+
   if(isset($_POST['submit']))
   {
     $username = $_POST['username'];
@@ -18,21 +19,24 @@ die("Connection Failed".mysqli_connect_error());
     $password = md5($password);
     $sql = "select * from users where name='$username' and password='$password'";
 
+    echo $username;
+
     if ($result=mysqli_query($conn,$sql))
       {
         $user_id=$result['user_id'];
         $rowcount=mysqli_num_rows($result);
         if($rowcount == 1)
         {
+          echo $user_id;
           setcookie('user_id',$user_id,time()+60*60*7);
           setcookie('user_name',$username,time()+60*60*7);
           //session_start();
           $_SESSION['user_id']=$user_id;
           //setcookie('userid',$user_id);
-          header("Location:/website/index.html");
+          header("Location:./index.html");
         }
         else {
-          header("Location:/website/login.html");
+          header("Location:./login.html");
         }
       }
 
